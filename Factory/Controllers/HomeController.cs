054f8327c49057f6e85354169f2aa08ec52c1d04
Lindsay Warr <iamalion@gmail.com>
2023-08-04 09:35:32 -0700

    WIP: Index.cshtml not working

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        ViewBag.PageTitle = "Dr. Silly Stringz";
-        return View();
-      }
+         private readonly FactoryContext _db;
+         public HomeController(FactoryContext db)
+        {
+            _db = db;
+        }
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            Machine[] machines = _db.Machines.ToArray();
+            Engineer[] engineers = _db.Engineers.ToArray();
+            Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+            model.Add("machines", machines);
+            model.Add("engineers", engineers);
+            ViewBag.PageTitle = "Dr. Silly Stringz";
+            return View();
+        }
 
     }
 }
