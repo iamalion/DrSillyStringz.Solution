3eda60bcbc3685969f00835481c436d3392f417f
Lindsay Warr <iamalion@gmail.com>
2023-08-04 08:35:51 -0700

    Add database

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+    public class Engineer 
+    {
+        public int EngineerId { get; set; }
+        [Display(Name = "Engineer Name: ")]
+        public string EngineerName { get; set; }
+    }
+}
---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+    public class Machine
+    {
+        public int MachineId { get; set; }
+        [Display(Name = "Machine Name: ")]
+        public string MachineName { get; set; }
+    
+    }
+}
