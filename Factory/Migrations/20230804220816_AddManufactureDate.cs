52ce7488e07d18975e9634219f757f83d7d9e81b
Lindsay Warr <iamalion@gmail.com>
2023-08-04 15:20:54 -0700

    Add required to MachineName and ManufactureDate

---
+++ Factory/Migrations/20230804220816_AddManufactureDate.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class AddManufactureDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Machines",
+                keyColumn: "MachineName",
+                keyValue: null,
+                column: "MachineName",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MachineName",
+                table: "Machines",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ManufactureDate",
+                table: "Machines",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ManufactureDate",
+                table: "Machines");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MachineName",
+                table: "Machines",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -10,9 +10,8 @@ namespace Factory.Models
         [Display(Name = "Machine Name: ")]
         [Required(ErrorMessage = "The name field can't be blank.")]
         public string MachineName { get; set; }
-        [Display(Name = "Manufacture date: ")]
+        [Display(Name = "Manufacture Date: ")]
         [Required(ErrorMessage = "The date can't be blank.")]
-        [DataType(DataType.Date)]
         public string ManufactureDate { get; set; }
         public int EngineerId { get; set; }
         public List<EngineerMachine> JoinEntities { get; }
